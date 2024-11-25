a91e7ad00f6635aec14ec16089abecc2d79f007b
Lapraf <vgolomovzyi@gmail.com>
2024-11-25 12:07:44 +0200

    v.1.1

--- dataclean/__init__.py
+++ dataclean/__init__.py
@@ -1,1 +1,21 @@
+# import pandas as pd
+# import numpy as np
+# from src.data_valydator import DataValidator
+# from src.feature_engineering import FeatureEngineering
+# from src.missing_data_handler import MissingDataHandler
+# from src.outliner_handler import OutlierHandler
+# from src.scaling_encoding_handler import ScalingEncodingHandler
+# from src.transformation_normalization_handler import TransformationNormalizationHandler
+# from abidas import DataClean
 
+__all__ = [
+    'DataValidator',
+    'FeatureEngineering',
+    'MissingDataHandler',
+    'OutlierHandler',
+    'ScalingEncodingHandler',
+    'TransformationNormalizationHandler',
+    'Dataclean'
+]
+
+__version__ = "1.0.0"
---
+++ dataclean/src/__init__.py
@@ -0,0 +1,21 @@
+# # import pandas as pd
+# import numpy as np
+# from data_valydator import DataValidator
+# from feature_engineering import FeatureEngineering
+# from missing_data_handler import MissingDataHandler
+# from outliner_handler import OutlierHandler
+# from scaling_encoding_handler import ScalingEncodingHandler
+# from transformation_normalization_handler import TransformationNormalizationHandler
+# from abidas import DataClean
+
+__all__ = [
+    'DataValidator',
+    'FeatureEngineering',
+    'MissingDataHandler',
+    'OutlierHandler',
+    'ScalingEncodingHandler',
+    'TransformationNormalizationHandler',
+    'Dataclean'
+]
+
+__version__ = "1.0.0"
